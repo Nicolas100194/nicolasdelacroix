7232fce8347ce8540109d86e643d12cbb7386e17
Nicolas <nicolasdelacroix10@gmail.com>
2024-02-02 15:17:33 +0100

    fix - responsive

--- header.php
+++ header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(''); ?></title>
     <?php wp_head()?>
 </head>
+
 <body>
     <header class="text">
         <a href="/" class="logo-header-desktop">
